ca9ea65ee238b0972078cb02ed7ecc4f795df8cc
Karlo Perkovic <perkovickarlo5@gmail.com>
2024-01-18 22:30:46 +0100

    seeder puni bazu

--- seeder.js
+++ seeder.js
@@ -1,11 +1,34 @@
 const axios = require('axios');
 const fs = require('fs');
 
+function delay(seconds) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, seconds * 1000); // Multiply by 1000 to convert seconds to milliseconds
+  });
+}
+
+
+async function fetchStationsInDB(){
+  try{
+    const response=await axios.get("http://localhost:5001/stations")
+    const dbStations =  response.data
+    const stationsByRadioBrowserId = {}
+    dbStations.forEach(s => {
+      stationsByRadioBrowserId[s.stationuuid] = s
+    })
+    return stationsByRadioBrowserId
+  }catch(e){
+    console.error('Error fetching stations from DB:', error.message);
+  }
+}
+
 async function fetchAllCountries() {
   try {
     const response = await axios.get('https://de1.api.radio-browser.info/json/countries');
     console.log("countries")
-    console.log(response.data)
+    console.log(`fetched total ${response.data.length} countries`)
     return response.data;
   } catch (error) {
     console.error('Error fetching countries:', error.message);
@@ -25,7 +48,16 @@ async function fetchStationsForCountry(countryCode) {
 }
 
 function filterStationsWithCoordinates(stations) {
-  return stations.filter(station => station.geo_lat && station.geo_long);
+  return stations.filter(station => {
+    return (
+      station.geo_lat 
+      && station.geo_long
+      && station.name
+      && station.stationuuid
+      && station.codec
+      && station.url
+    )
+  })
 }
 
 function mergeStations(countryStations, allStations) {
@@ -49,21 +81,57 @@ function limitStations(stations,limit){
     return stations.splice(0,limit)
 }
 
+async function updateStation(station, ourId){
+  try {
+    const response = await axios.put('http://localhost:5001/stations/'+ourId, station);
+  } catch (error) {
+    
+    console.error('Error in update station:', station, error);
+  }
+}
+
+async function insertStation(station){
+  try {
+    const response = await axios.post('http://localhost:5001/stations/', station);
+  } catch (error) {
+    console.error('Error in insert station:', station);
+  }
+}
+
+async function updateOurServer(stanice, stationsByRadioBrowserId) {
+  stanice.forEach( async stanica => {
+    const radioBrowserId = stanica.stationuuid
+    const stanicaModified = {...stanica, 
+                              lat: stanica.geo_lat,
+                              lng: stanica.geo_long,
+                              lastchangetime_in_radiobrowser: stanica.lastchangetime_iso8601
+                            }
+    // check if station is already in our server, if yes update it, else insert it as new
+    if (radioBrowserId in stationsByRadioBrowserId) {
+      const ourId = stationsByRadioBrowserId[radioBrowserId]._id
+      await updateStation(stanicaModified, ourId)
+    } else {
+      await insertStation(stanicaModified)
+    }
+  })
+}
+
+
 async function main() {
   try {
     // Fetch all countries
     const countries = await fetchAllCountries();
-    //return
+    const stationsByRadioBrowserId = await fetchStationsInDB()
     // Fetch and merge stations for each country
     let allStations = [];
     for (const country of countries) {
       const countryStations = await fetchStationsForCountry(country.iso_3166_1);
       const filteredStations = filterStationsWithCoordinates(countryStations);
       const limitirane = limitStations(filteredStations, 15);
+      updateOurServer(limitirane, stationsByRadioBrowserId)
       allStations = mergeStations(limitirane, allStations);
+      await delay(1)
     }
-    
-
     // Save the final list of stations to a file
     await saveStationsToFile(allStations);
   } catch (error) {
